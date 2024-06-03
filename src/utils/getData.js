de6359854db4ff729a5909ac4db25ebadb6667ee
DESKTOP-O519K1L\mindi <gordon70705@gmail.com>
2024-06-03 09:53:31 +0800

    Update change Month

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -5,24 +5,24 @@ import { reactive } from "vue";
 const workTime = reactive([]);
 
 const getData = async (id) => {
-	let QueryData = new FormData();
-	let month = 5;
-	let year  = 2024;
-	QueryData.append("year", year);
-	QueryData.append("month", month);
+  let QueryData = new FormData();
+  let month = 6;
+  let year = 2024;
+  QueryData.append("year", year);
+  QueryData.append("month", month);
 
-	workTime.length = 0;
-	await axiosApi
-		.default({
-			method: "QUERY",
-			url: `/打卡紀錄/${id}`,
-			data: QueryData
-		})
-		.then((res) => {
-			res.data.data.map((item) => {
-				workTime.push(item);
-			});
-		});
+  workTime.length = 0;
+  await axiosApi
+    .default({
+      method: "QUERY",
+      url: `/打卡紀錄/${id}`,
+      data: QueryData,
+    })
+    .then((res) => {
+      res.data.data.map((item) => {
+        workTime.push(item);
+      });
+    });
 };
 
 export { getData, workTime };
